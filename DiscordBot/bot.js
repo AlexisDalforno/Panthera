b94e9ade89bb7aaa6ba60fc06dcdbec5888c7216
Alexis Dalforno <alexis.dalforno@gmail.com>
2019-08-14 20:58:20 -0700

    Add Random Messages for Greeting

--- DiscordBot/bot.js
+++ DiscordBot/bot.js
@@ -2,7 +2,8 @@
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
-var random = Math.floor((Math.random() * 3) + 1);
+var random = Math.floor((Math.random() * 5) + 1);
+var introMessage;
 
 // [ L O G G E R   S E T T I N G S ]
 logger.remove(logger.transports.Console);
@@ -23,10 +24,31 @@ bot.on('ready', function (evt) {
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
 
+// [ G E T   U S E R   O B J E C T ]
+function get_user(memberID) {
+    var user = bot.users[memberID];
+    return user;
+}
+
 // [ W E L C O M E   P L A Y E R ]
 bot.on('guildMemberAdd', function (member) {
+    var joiner = get_user(member.id);
+    var name =  joiner.username;
+
+    if(random == 1){
+        introMessage = 'Welcome to the clan ' + name + '!';
+    } else if(random == 2){
+        introMessage = name + ' is here ready to eat some eggplant.';
+    } else if(random == 3){
+        introMessage = 'WAZZZZZZZZZUUUUUUUUUUPPPPPPPPPPPPPPPP ' + name + '!';
+    } else if(random == 4){
+        introMessage = 'Touch my holdyflaps ' + name;
+    } else if(random == 5){
+        introMessage = 'A wild ' + name + ' has appeared.';
+    }
+
     bot.sendMessage({
         to: '609978851778363404',
-        message: 'Welcome to the server!'
+        message: introMessage
     });
 });
