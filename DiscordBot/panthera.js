714414156d96ae88de218d6786a045a639feb620
Alexis Dalforno <alexis.dalforno@gmail.com>
2020-06-19 23:10:56 -0700

    Added Rest of Character Setup Methods

--- DiscordBot/panthera.js
+++ DiscordBot/panthera.js
@@ -30,6 +30,18 @@ client.on('message', message => {
             newCharacter(message, args);
             break;
 
+        case "del-char":
+            delCharacter(message);
+            break;
+
+        case "char-stats":
+            setStats(message, args);
+            break;
+
+        case "char-inv":
+            setInv(message, args);
+            break;
+
         case "help":
         default:
             commands(message);
@@ -50,8 +62,7 @@ function roll(message, args) {
 
     if (args[0].length > 1 && !isNaN(args[0].substr(1))) {
         diceRoll = args[0].substr(1);
-        if (!dice.includes(diceRoll)) 
-            return message.channel.send(`${args[0]} is not a valid dice type.`); 
+        if (!dice.includes(diceRoll)) return message.channel.send(`${args[0]} is not a valid dice type.`); 
     }
 
     if (args.length > 1 && !isNaN(args[1])) quantity = Math.floor(args[1]);
@@ -77,7 +88,16 @@ function commands(message) {
                         "*Will roll [quantity] dice of [number] sides + [modifier]. Defaults to 1 d20 + 0. Valid dice: 4, 6, 8, 10, 12, 20.*\n" +
                         "--------------\n" + 
                         "`!new-char name age race m/f` \n" +
-                        "*Creates new character with given info. Age must be number representation between 17-1000 e.g. 20. For sex provide \"m\" or \"f\".*");
+                        "*Creates new character with given info. Age must be number representation between 17-1000 e.g. 20. For sex provide \"m\" or \"f\".*\n" +
+                        "--------------\n" + 
+                        "`!del-char` \n" +
+                        "*Deletes character from database.*\n" +
+                        "--------------\n" + 
+                        "`!char-stats health str int dex char` \n" +
+                        "*Adds player health and sets stats. Give each stat a number between 1-4 as a ranking of where you want most points to go towards.*" +
+                        "--------------\n" + 
+                        "`!char-inv gold [item1] : [item2]` \n" +
+                        "*Sets initial inventory with amount of gold and the option of two separate items. Separate each item with a colon.*");
 }
 
 // [ C H A R A C T E R   C R E A T E ]
@@ -91,30 +111,27 @@ function newCharacter(message, args) {
         var characterArr = JSON.parse(data); 
 
         for (var i = 0; i < characterArr.charas.length; i++) {
-            if(user in characterArr.charas[i]){
+            if(user in characterArr.charas[i]) {
                 message.channel.send("You already have a character! If you would like to make a new one, " +
                                         "please use `!del-char` and try again.");
                 return;
             }
         }
 
-        if (args.length != 4) {
-            return message.channel.send("`usage: !new-char name age race m/f`");
-        }
+        if (args.length != 4) return message.channel.send("`usage: !new-char name age race m/f`");
     
         name = args[0];
     
-        if (isNaN(args[1]) || args[1] > 1000 || args[1] < 17) {
+        if (isNaN(args[1]) || args[1] > 1000 || args[1] < 17) 
             return message.channel.send(`${args[1]} is invalid age argument. Please give a numerical value between 17-1000.`);
-        }
+
     
         age = Math.floor(args[1]);
         race = args[2];
     
-        if (args[3] != 'm' && args[3] != 'f') {
+        if (args[3] != 'm' && args[3] != 'f') 
             return message.channel.send(`${args[3]} is invalid sex. Use only m or f for sex argument.`);
-        }
-    
+
         sex = args[3];
 
         let chara = {
@@ -142,11 +159,119 @@ function newCharacter(message, args) {
 
         characterArr.charas.push(chara);
     
-        fs.writeFile('./characters.json', JSON.stringify(characterArr, null, 2), 'utf-8', function(err) {
-            if (err) throw err
-        });
+        fs.writeFile('./characters.json', JSON.stringify(characterArr, null, 2), 'utf-8', function(err) {if (err) throw err});
 
         message.channel.send(`Welcome to the team ${name}! Use the command \`!char-stats\` to set stats and \`!char-inv\` to fill your inventory.`);
+    });
+}
+
+// [ D E L E T E   C H A R A C T E R ]
+function delCharacter(message) {
+    fs.readFile('./characters.json', 'utf-8', function(err, data) {
+        if (err) throw err;
+
+        var user = message.author.username;
+        var characterArr = JSON.parse(data); 
+        var removed = false;
+
+        for (var i = 0; i < characterArr.charas.length; i++) {
+            if(user in characterArr.charas[i]) {
+                characterArr.charas.splice(i, 1);
+                removed = true;
+            }
+        }
+
+        fs.writeFile('./characters.json', JSON.stringify(characterArr, null, 2), 'utf-8', function(err) {if (err) throw err});
+
+        if (removed) message.channel.send("Your character has been successfully removed!");
+        else message.channel.send("You do not have a character in the database.");
+    });
+}
+
+// [ S E T   C H A R A C T E R   S T A T S ]
+function setStats(message, args) {
+    if (args.length != 5) {
+        return message.channel.send(`You didn't provide correct arguments, ${message.author}! \n` +
+        `\`usage: !char-stats health str int dex char\``);
+    }
+
+    var health = 0, str = 0, int = 0, dex = 0, char = 0;
+
+    if (!isNaN(args[0])) health = Math.floor(args[0]);
+    if (!isNaN(args[1])) str = Math.floor(args[1]);
+    if (!isNaN(args[2])) int = Math.floor(args[2]);
+    if (!isNaN(args[3])) dex = Math.floor(args[3]);
+    if (!isNaN(args[4])) char = Math.floor(args[4]);
+
+    if (!health || !str || !int || !dex || !char) {
+        return message.channel.send(`You didn't provide correct arguments, ${message.author}! \n` +
+                                    `\`usage: !char-stats health str int dex char\``);
+    }
+
+    if (str + int + dex + char != 10) return message.channel.send("Each stat must be uniquely ranked 1-4.");
+    if (health > 200) return message.channel.send("Health must be below 200 healthpoints.");
+
+    fs.readFile('./characters.json', 'utf-8', function(err, data) {
+        if (err) throw err;
+
+        var user = message.author.username;
+        var characterArr = JSON.parse(data); 
+
+        for (var i = 0; i < characterArr.charas.length; i++) {
+            if(user in characterArr.charas[i]) {
+                characterArr.charas[i][user].stats.health = health;
+                characterArr.charas[i][user].stats.strength = Math.floor(Math.random() * str) + str;
+                characterArr.charas[i][user].stats.intelligence = Math.floor(Math.random() * int) + int;
+                characterArr.charas[i][user].stats.dexterity = Math.floor(Math.random() * dex) + dex;
+                characterArr.charas[i][user].stats.charisma = Math.floor(Math.random() * char) + char;
+            }
+        }
+
+        fs.writeFile('./characters.json', JSON.stringify(characterArr, null, 2), 'utf-8', function(err) {if (err) throw err});
+
+        message.channel.send("Stats successfully set!");
+    });
+}
+
+// [ S E T   C H A R A C T E R  I N V E N T O R Y ]
+function setInv(message, args) {
+    if (!args.length) {
+        return message.channel.send(`You didn't provide gold count, ${message.author}! \n` +
+        `\`usage: !char-inv gold [item1] : [item2]\``);
+    }
+
+    var gold = 0, item1 = "", item2 = "", change = false;
+
+    if (!isNaN(args[0])) gold = Math.floor(args[0]);
+    else return message.channel.send("Please enter a numerical value for your gold.");
+
+    if (gold > 30) return message.channel.send("You cannot be that rich. I would say you look to be the holder of only 30 or less shillings.");
+
+    for (var i = 1; i < args.length; i++) {
+        if (args[i] == ':') change = true;
+        else if (change) item2 = item2.concat(args[i], " ");
+        else item1 = item1.concat(args[i], " ");
+    }
+
+    fs.readFile('./characters.json', 'utf-8', function(err, data) {
+        if (err) throw err;
+
+        var user = message.author.username;
+        var characterArr = JSON.parse(data); 
+
+        for (var i = 0; i < characterArr.charas.length; i++) {
+            if(user in characterArr.charas[i]) {
+                characterArr.charas[i][user].items.gold = gold;
+                if (item1.length)
+                    characterArr.charas[i][user].items['item1'] = item1.trim();
+                if (item2.length)
+                    characterArr.charas[i][user].items['item2'] = item2.trim();
+            }
+        }
+
+        fs.writeFile('./characters.json', JSON.stringify(characterArr, null, 2), 'utf-8', function(err) {if (err) throw err});
+
+        message.channel.send("Inventory successfully filled!");
     });
 }
 
